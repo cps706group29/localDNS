fd43b8673bd9eacd47176c352b45c884e8e08222
Philam18 <philiplam818@gmail.com>
2018-04-11 08:36:46 -0400

    updated

--- LocalDNS.java
+++ LocalDNS.java
@@ -7,23 +7,28 @@ public class LocalDNS{
     byte[] receiveData = new byte[1024];
     byte[] sendData = new byte[1024];
 
+    // LocalDNS runs infinitely, listens for any incoming packets
     while(true){
+      // incoming packet arrives at the socket
       DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-
       serverSocket.receive(receivePacket);
 
-      String sentence = new String(receivePacket.getData());
+      String requestURL = new String(receivePacket.getData());
+      System.out.println("Incoming Message: " + requestURL);
 
+      // Prepare a response
       InetAddress IPAddress = receivePacket.getAddress();
-
       int port = receivePacket.getPort();
 
-      String response = sentence;
+      ///////////////////////// Take requestURL from UDP message, and resolve it ///////////////////////
+      
 
+      //////////////////////////////////////////////////////////////////////////////////////
+      String response = requestURL;
       sendData = response.getBytes();
-
       DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
 
+      // Send the Response
       serverSocket.send(sendPacket);
     }
   }
