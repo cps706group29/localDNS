d411ef511570fdc03fa5befbd3bfe918a83559a0
Philam18 <philiplam818@gmail.com>
2018-04-11 08:07:10 -0400

    update

---
+++ LocalDNS.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.*;
+
+class LocalDNS{
+  public static void main(String argv[]) throws Exception{
+    DatagramSocket serverSocket = new DatagramSocket(40280);
+    byte[] receiveData = new byte[1024];
+    byte[] sendData = new byte[1024];
+
+    while(true){
+      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+      serverSocket.receive(receivePacket);
+
+      String sentence = new String(receivePacket.getData());
+
+      InetAddress IPAddress = receivePacket.getAddress();
+
+      int port = receivePacket.getPort();
+
+      String response = sentence;
+
+      sendData = response.getBytes();
+
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+
+      serverSocket.send(sendPacket);
+    }
+  }
+}
